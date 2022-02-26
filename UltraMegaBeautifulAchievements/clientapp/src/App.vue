485be1b499b6b52cec684c7acafa0634ac6495f1
Дмитрий Чернов <4eduma4@gmail.com>
2022-02-26 10:48:55 +0300

    build пересобрал ui

--- UltraMegaBeautifulAchievements/clientapp/src/App.vue
+++ UltraMegaBeautifulAchievements/clientapp/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-export default {
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data () {
+    return {
+      //
+    }
+  }
+})
+</script>
