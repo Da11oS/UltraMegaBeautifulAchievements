5f768d61b8b4443b6fdf0146f8443a4994b012e1
Maxim <mxm1337@rambler.ru>
2022-04-14 21:31:52 +0300

    докоммит прошлого

---
+++ Achievements.Database/Migrations/20220414183102_user role default = 1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Achievements.Database.Migrations
+{
+    public partial class userroledefault1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
